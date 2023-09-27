1231dc8d6c6930bfb623b1d97cf43175d566993f
Kobayasi <8066248+myjiee@user.noreply.gitee.com>
2023-09-27 10:28:22 +0800

    压缩背景图片

--- src/main.js
+++ src/main.js
@@ -1,16 +1,13 @@
 import { createApp } from 'vue'
 // import './style.css'
 import App from './App.vue'
-
-import ElementPlus from 'element-plus'; //为vue3项目特别更新的版本
+import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
-
 import 'element-plus/theme-chalk/dark/css-vars.css'
 import 'vue-json-pretty/lib/styles.css';
 import JsonViewer from "vue3-json-viewer"
 import dayjs from 'dayjs'
-
-
+import axios from 'axios'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 const app = createApp(App)
@@ -32,7 +29,6 @@ app.mixin({
     }
 })
 
-import axios from 'axios'
 // axios.defaults.baseURL = '/api'
 app.config.globalProperties.$http = axios
 
